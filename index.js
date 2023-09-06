4a08809a2d0726267fb286205eeb6cad46824773
nurealammiaji <nurealam.miaji@gmail.com>
2023-09-06 22:16:32 +0600

    cors added

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
 const expres = require("express");
+const cors = require("cors");
 const app = expres();
 const port = 5000;
+
+app.use(cors());
 
 app.get("/", (req, res) => {
     res.send("Travel Buzz Server");
 })
 
 app.listen(port, () => {
-    console.log("Travel Buzz Server Running:", port);
+    console.log("Travel Buzz Server Running on Port:", port);
 })
